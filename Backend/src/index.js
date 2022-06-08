282ce2cb0fd10af332ec2b4e1fafb1a1671bf718
unknown <you@example.com>
2022-06-08 10:12:26 -0500

    changes in index (Backend)

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -1,14 +1,23 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const http = require('http')
+const cors = require('cors')
+const socketio = require('socket.io')
 
-mongoose.connect(
-  `mongodb+srv://admin:fd3K!-G6tv3mG9N@line-chat-db.jhyon.mongodb.net/?retryWrites=true&w=majority`
-);
+const PORT = process.env.PORT || 5000
 
 const app = express();
+
+const server = http.createServer(app)
+mongoose.connect(
+  `mongodb+srv://admin:fd3K!-G6tv3mG9N@line-chat-db.jhyon.mongodb.net/?retryWrites=true&w=majority`
+
+);
+
+
 app.get("/", (req, res) => {
   res.send("Hi, my name is Chiki!");
 });
-app.listen(3001, () => {
-  console.log("Server has started at port 3001!");
+server.listen(PORT, () => {
+  console.log('Server is up and running on port ' + PORT);
 });
