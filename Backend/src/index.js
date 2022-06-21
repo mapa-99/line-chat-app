ee36da459a8eb1b0519fc5f70cb742cc9b63b89f
Manuel Paredes <mapa991@hotmail.com>
2022-06-21 10:39:29 -0500

    Backend complete!

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -1,23 +1,72 @@
 const express = require("express");
 const mongoose = require("mongoose");
-const http = require('http')
-const cors = require('cors')
-const socketio = require('socket.io')
+const http = require("http");
+const cors = require("cors");
+const socketio = require("socket.io");
 
-const PORT = process.env.PORT || 5000
-
+const PORT = process.env.PORT || 5000;
 const app = express();
+app.use(cors());
 
-const server = http.createServer(app)
+const server = http.createServer(app);
+
+//Real time data functionality
+const io = socketio(server, { wsEngine: "ws" });
+
+io.on("connection", (socket) => {
+  //The user join the room
+  socket.on("join", (data) => {
+    const { name, room } = data;
+    const { user, error } = addUser({ id: socket.id, name, room });
+
+    if (error) return;
+    socket.emit("message", {
+      user: "admin",
+      text: `${user.name}, it's great to see you in here.`,
+    });
+    socket.broadcast.to(user.room).emit("message", {
+      user: "admin",
+      text: `${user.name} has just landed to the room.`,
+    });
+    socket.join(user.room);
+    io.to(user.room).emit("room-data", {
+      room: user.room,
+      users: getAllUsers(user.room),
+    });
+  });
+
+  //The user left the room
+  socket.on("left", () => {
+    const user = removeUser(socket.id);
+    user &&
+      io.to(user.room).emit("message", {
+        user: "admin",
+        text: `${user.name} has just left!`,
+      });
+  });
+  
+  //The user send a message
+  socket.on("send-message", (message) => {
+    const user = getUser(socket.id);
+    io.to(user.room).emit("message", {
+      user: user.name,
+      text: message,
+    });
+    io.to(user.room).emit("room-data", {
+      room: user.room,
+      users: getAllUsers(user.room),
+    });
+  });
+});
+
+//Connect to DB
 mongoose.connect(
   `mongodb+srv://admin:fd3K!-G6tv3mG9N@line-chat-db.jhyon.mongodb.net/?retryWrites=true&w=majority`
-
 );
 
-
 app.get("/", (req, res) => {
-  res.send("Hi, my name is Chiki!");
+  res.send("It's working!");
 });
 server.listen(PORT, () => {
-  console.log('Server is up and running on port ' + PORT);
+  console.log("Server is up and running on port " + PORT);
 });
---
+++ Backend/src/utils.js
@@ -0,0 +1,24 @@
+let users = [];
+
+const addUser = ({ id, name, room }) => {
+  name = name.trim().toLowerCase();
+  room = room.trim().toLowerCase();
+
+  const userExist = users.find(
+    (user) => user.name === name && user.room === room
+  );
+
+  if (!user || !room || userExist) return { error: "error" };
+  const user = { id, name, room };
+  users = { ...users, user };
+  return { user };
+};
+
+const removeUser = (id) => {
+  const i = users.findIndex((user) => user.id === id);
+  return i !== -1 ? users.splice(i, 1)[0] : null;
+};
+
+const getUser = (id) => users.filter((user) => user.id === id)[0];
+const getAllUsers = (room) => users.filter((user) => user.room === room);
+module.exports = { addUser, removeUser, getAllUsers, getUser };
